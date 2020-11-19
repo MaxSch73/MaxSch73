940b614ea73cbdc727c99ed7d19b4834907e905c
Max <schuller.m04@htlwienwest.at>
2020-11-19 08:39:43 +0100

    Commit

--- jss/v1.js
+++ jss/v1.js
@@ -1,62 +1,39 @@
-const cards = document.querySelectorAll('#a1,#b1,#c1,#d1,#e1,#f1,#g1,#h1')
 
-let hasFlipped = false
-let lock = false
-let firstCard, secondCard
 
-function flipcard(){
-    if(lock) return
-    if(this === firstCard) return
-    this.classList.toggle('flip')
 
-    if(!hasFlipped){
-        hasFlipped = true
-        firstCard = this
-        
-    }
-    else{
-        hasFlipped = false
-        secondCard = this
 
-        checkIfMatch()
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+carddeck = document.getElementById('card-deck');
+for(var i=0; i<16; i++){
+    var Div = document.createElement('div')
+    Div.className = 'card'+i;
+    document.body.appendChild(Div)
+
+    container.innerHTML += '<div class="card">ITEM' + (i + 1) + '</div>';
 }
-
-function checkIfMatch(){
-    if(firstCard.dataset.framework === secondCard.dataset.framework)
-        {
-           disableCards() 
-        }
-        else{
-            unflipCards();
-        }
-}
-function disableCards(){
-    firstCard.removeEventListener('click',flipcard)
-    secondCard.removeEventListener('click',flipcard)
-    reset()
-}
-function unflipCards(){
-    lock = true
-    setTimeout(() =>{
-    firstCard.classList.remove('flip')
-    secondCard.classList.remove('flip')
-
-    lock = false
-    },1500);
-}
-function reset(){
-    [hasFlipped, lock] = [false, false]
-    [firstCard, secondCard] = [null, null]
-}
-
-(function shuffle(){
-    cards.forEach(card =>{
-        let random = Math.floor(Math.random() * 16)
-        card.style.order = random
-    })
-})()
-
-
-
-cards.forEach(card => card.addEventListener('click',flipcard))
